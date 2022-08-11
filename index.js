cfc5385ea8eb10447702d453df3de7e6323693a0
Nabeel Hussain M N <mnnh2001@gmail.com>
2022-08-11 16:17:45 +0530

    update Procfile

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(express.json({ extended: false }));
 app.use("/", require("./routes/index"));
 app.use("/api/url", require("./routes/url"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
