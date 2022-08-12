8f8806aad496d13865a77271e4c4746419a1aaaa
Nabeel Hussain M N <mnnh2001@gmail.com>
2022-08-12 09:36:49 +0530

    added TTL to mongo collection

--- models/Url.js
+++ models/Url.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema(
-  {
-    urlCode: String,
-    longUrl: String,
-    shortUrl: String,
-    date: { type: String, default: Date.now },
-  },
-  { timestamps: true }
-);
-urlSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 });
+const urlSchema = new mongoose.Schema({
+  urlCode: String,
+  longUrl: String,
+  shortUrl: String,
+  date: { type: String, default: Date.now },
+  createdAt: { type: Date, expires: 60, default: Date.now },
+});
+// urlSchema.index({ createdAt: 1 }, { expireAfterSeconds: 60 });
 
 module.exports = mongoose.model("Url", urlSchema, "Urls");
