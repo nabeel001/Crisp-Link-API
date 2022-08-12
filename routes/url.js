97b33f85af70711b3fdcc52878e65a829ac2aea6
Nabeel Hussain M N <mnnh2001@gmail.com>
2022-08-12 15:26:23 +0530

    updated to nanoid

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const validUrl = require("valid-url");
-const shortid = require("shortid");
+const { nanoid } = require("nanoid");
 
 const Url = require("../models/Url");
 
@@ -13,7 +13,7 @@ router.post("/shorten", async (req, res) => {
   const baseUrl = process.env.HOST_URI;
 
   //create url code
-  const urlCode = shortid.generate();
+  const urlCode = nanoid(9);
 
   //check long url is valid
   if (validUrl.isUri(longUrl)) {
