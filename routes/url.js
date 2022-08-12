71b858fe8298e2630346b6485596c114a7bf0a56
Nabeel Hussain M N <mnnh2001@gmail.com>
2022-08-12 15:32:08 +0530

    updated to nanoid

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const validUrl = require("valid-url");
-const { nanoid } = require("nanoid");
+import { nanoid } from "nanoid";
 
 const Url = require("../models/Url");
 
