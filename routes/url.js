c9b17be1d7177ef9d9fd4678f1882e55dd26951b
Nabeel Hussain M N <mnnh2001@gmail.com>
2022-08-12 10:47:12 +0530

    added TTL to mongo collection

--- routes/url.js
+++ routes/url.js
@@ -20,7 +20,7 @@ router.post("/shorten", async (req, res) => {
     try {
       let url = await Url.findOne({ longUrl });
       if (url) {
-        res.json(url);
+        res.status(200).json(url);
       } else {
         const shortUrl = baseUrl + "/" + urlCode;
         url = new Url({
@@ -32,7 +32,7 @@ router.post("/shorten", async (req, res) => {
 
         await url.save();
 
-        res.json(url);
+        res.status(200).json(url);
       }
     } catch (err) {
       console.error(err.message);
